f216fcaa82a68ddaff434cbd30a735d8b591032f
xuesongdkl <13233490803@163.com>
2019-03-22 03:30:00 +0000

    denug

--- app/Http/Controllers/User/IndexController.php
+++ app/Http/Controllers/User/IndexController.php
@@ -72,7 +72,7 @@ class IndexController extends Controller
         $curl = curl_init();                                        //初始化
         curl_setopt($curl, CURLOPT_URL,$url);                       //设置抓取的url
         curl_setopt($curl, CURLOPT_POST, 1);                        //设置post方式提交
-        curl_setopt($curl, CURLOPT_POSTFIELDS,['data'=>$data]);
+        curl_setopt($curl, CURLOPT_POSTFIELDS,$data);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);              //设置获取的信息以文件流的形式返回，而不是直接输出
         curl_setopt($curl, CURLOPT_HEADER, 0);                      //设置头文件的信息作为数据流输出
         $rs = curl_exec($curl);
